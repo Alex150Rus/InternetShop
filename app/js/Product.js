fc20956ad2264a9ad291de075bbc2e9879514d3a
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-08 10:46:14 +0300

    class Product js was modified for this project products

--- app/js/Product.js
+++ app/js/Product.js
@@ -1,44 +1,61 @@
-class Product{
-    constructor(id, title, price, img = 'https://placehold.it/200x150', container = '#products'){
-        this.id = id;
-        this.price = price;
-        this.title = title;
-        this.img = img;
-        this.container = container;
-        this._render(this.container);
-    }
-    _render(container){
-        let $wrapper = $('<div/>', {
-            class: 'product'
-        });
+class Product {
+  constructor(id, title, price, img = 'https://placehold.it/200x150', container = '#products') {
+    this.id = id;
+    this.price = price;
+    this.title = title;
+    this.img = img;
+    this.container = container;
+    this._render(this.container);
+  }
 
-        let $img = $('<img/>', {
-            src: this.img
-        });
-        let $desc = $('<div/>', {
-            class: 'desc'
-        });
-        let $name = $('<p/>', {
-            text: this.title
-        });
-        let $price = $(`<p>Цена: <span class="product-price">${this.price}</span></p>`);
+  _render(container) {
+    let $wrapper = $('<div/>', {
+      class: 'product'
+    });
 
-        let $buyBtn = $('<button/>', {
-            class: 'buyBtn',
-            text: 'Купить',
-            'data-id': this.id,
-            'data-name': this.title,
-            'data-price': this.price
-        });
+    let $linkWrapperImg = $('<a/>', {
+      class: 'product__link',
+      href: '#',
+    });
+
+    let $img = $('<img/>', {
+      class: 'product__image',
+      alt: this.title,
+      src: this.img
+    });
+
+    let $name = $('<h2/>', {
+      class: 'product__name',
+      text: this.title
+    });
+    let $price = $('<h2/>', {
+      class: 'product__price',
+      text: `$ ${Number(this.price).toFixed(2)}`
+    });
+
+    let $buyBtn = $('<div/>', {
+      class: 'product__cartbuttonflex product__invisiblebutton',
+      'data-id': this.id,
+      'data-name': this.title,
+      'data-price': this.price
+    });
+
+    let $buyBtnIcon = $('<i/>', {
+      class: 'product__cart-image fas fa-shopping-cart',
+      text: 'Add to Cart'
+    });
 
 
-        // Собираем структуру html
-        $img.appendTo($wrapper);
-        $name.appendTo($desc);
-        $price.appendTo($desc);
-        $buyBtn.appendTo($desc);
-        $desc.appendTo($wrapper);
+    // Собираем структуру html
+    $img.appendTo($linkWrapperImg);
+    $name.appendTo($linkWrapperImg);
+    $price.appendTo($linkWrapperImg);
+    $linkWrapperImg.appendTo($wrapper);
 
-        $(container).append($wrapper);
-    }
+
+    $buyBtnIcon.appendTo($buyBtn);
+    $buyBtn.appendTo($wrapper);
+
+    $(container).append($wrapper);
+  }
 }
