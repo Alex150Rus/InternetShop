1b1e6db9a328bc29018b3078bb012c2bcf7297b4
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-08 10:04:01 +0300

    renamed and modified a little

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
     //Создавать товары
-    let product1 = new Product(123, 'Ноутбук', 45600);
-    let product2 = new Product(124, 'Клавиатура', 1200);
-    let product3 = new Product(125, 'Мышь', 600);
+    let product1 = new Product(123, 'man-white', 52, 'img/Item1.png', '.featureditemsmesh');
+    let product2 = new Product(124, '', 1200, 'img/Item2.png', '.featureditemsmesh');
+    let product3 = new Product(125, 'Мышь', 600, 'img/Item3.png', '.featureditemsmesh');
 
     //Корзина
-    let mycart = new Cart('getCart.json');
+    /*let mycart = new Cart('getCart.json');
 
     //Обработчик
     $('#products').on('click', '.buyBtn', e => {
@@ -19,4 +19,5 @@ $(document).ready(() => {
       mycart.remove($CurrentProduct)
   });
     console.log(mycart);
+    */
 });
