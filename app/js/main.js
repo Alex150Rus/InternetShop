092055c90cab7374050f4dd8126196a26a179ba2
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-08 10:49:18 +0300

    continue adding products, quantity of spaces was reduced to 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,24 @@
 $(document).ready(() => {
-    //Создавать товары
-    let product1 = new Product(123, 'man-white', 52, 'img/Item1.png', '.featureditemsmesh');
-    let product2 = new Product(124, '', 1200, 'img/Item2.png', '.featureditemsmesh');
-    let product3 = new Product(125, 'Мышь', 600, 'img/Item3.png', '.featureditemsmesh');
+  //Создавать товары
+  let product1 = new Product(123, 'man-white', 52.00, 'img/Item1.png', '.featureditemsmesh');
+  let product2 = new Product(124, 'girl-rose', 52.00, 'img/Item2.png', '.featureditemsmesh');
+  let product3 = new Product(125, 'man-blue', 52.00, 'img/Item3.png', '.featureditemsmesh');
+  let product4 = new Product(126, 'girl-flowers', 52.00, 'img/Item4.png', '.featureditemsmesh');
 
-    //Корзина
-    /*let mycart = new Cart('getCart.json');
+  //Корзина
+  /*let mycart = new Cart('getCart.json');
 
-    //Обработчик
-    $('#products').on('click', '.buyBtn', e => {
-        mycart.addProduct(e.target);
-        console.log(e.target);
-    });
-
-    //ещё один обработчик
-  $('.cart-items-wrap').on('click', '.deleteBtn', event=> {
-      let $CurrentProduct = $(event.target);
-      mycart.remove($CurrentProduct)
+  //Обработчик
+  $('#products').on('click', '.buyBtn', e => {
+      mycart.addProduct(e.target);
+      console.log(e.target);
   });
-    console.log(mycart);
-    */
+
+  //ещё один обработчик
+$('.cart-items-wrap').on('click', '.deleteBtn', event=> {
+    let $CurrentProduct = $(event.target);
+    mycart.remove($CurrentProduct)
+});
+  console.log(mycart);
+  */
 });
