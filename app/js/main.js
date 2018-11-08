0e34508536d4c6f8b9287ab895a98cac4a62ef11
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2018-11-08 10:58:56 +0300

    all products added

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ $(document).ready(() => {
   let product2 = new Product(124, 'girl-rose', 52.00, 'img/Item2.png', '.featureditemsmesh');
   let product3 = new Product(125, 'man-blue', 52.00, 'img/Item3.png', '.featureditemsmesh');
   let product4 = new Product(126, 'girl-flowers', 52.00, 'img/Item4.png', '.featureditemsmesh');
+  let product5 = new Product(127, 'girl-stripes', 52.00, 'img/Item5.png', '.featureditemsmesh');
+  let product6 = new Product(128, 'man-hat', 52.00, 'img/Item6.png', '.featureditemsmesh');
+  let product7 = new Product(129, 'man-belt', 52.00, 'img/Item7.png', '.featureditemsmesh');
+  let product8 = new Product(130, 'man-blue-hat', 52.00, 'img/Item8.png', '.featureditemsmesh');
 
   //Корзина
   /*let mycart = new Cart('getCart.json');
